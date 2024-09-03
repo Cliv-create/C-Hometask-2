78dab69be9c0dc0ac3f94c66b0eab92b08a2e8ed
User <User@WIN-7SIR4INGLOJ>
2024-09-03 11:37:50 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    double user_money = 0;
+
+    cout << "Please insert the amount of money you have (Example: 26.78): ";
+    cin >> user_money;
+
+    int Money = (int)user_money;
+    int Change = (user_money - Money) * 100;
+    cout << "Here's amount of money that you have: " << Money << "\n" << "Here's amount of change that you have: " << Change << "\n";
+    */
+
+    /*
+    int Completed = 0;
+    int Missing = 0;
+
+    cout << "Percentage of students who completed hometask calculator\n";
+
+    cout << "Insert number of student who completed the hometask: ";
+    cin >> Completed;
+    
+    cout << "Insert number of students who did not complete the hometask: ";
+    cin >> Missing;
+    
+    double PercentageOfCompletedDouble = (Completed / double(Completed + Missing)) * 100;
+    cout << "Percentage of completed works: " << PercentageOfCompletedDouble;
+    */
+
+    
+
+    /*
+    int NumberInt = 0;
+    int NumberToPowerInt = 0;
+
+    cout << "Number to the power calculator\n";
+
+    cout << "Please insert number: ";
+    cin >> NumberInt;
+
+    cout << "Please insert the number for power of " << NumberInt << ": ";
+    cin >> NumberToPowerInt;
+
+    cout << pow(NumberInt, NumberToPowerInt);
+    */
+
+
+
+    /*
+    int FirstInt = 0;
+    int SecondInt = 0;
+    int ThirdInt = 0;
+    cout << "Arithmetic average number calculator\n";
+    cout << "Please insert first number: ";
+    cin >> FirstInt;
+    cout << "Please insert second number: ";
+    cin >> SecondInt;
+    cout << "Please insert third number: ";
+    cin >> ThirdInt;
+    cout << "Arithmetic average number: " << (FirstInt + SecondInt + ThirdInt) / 3;
+    */
+}
